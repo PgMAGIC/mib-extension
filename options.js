8b8ea2a0a85bf0498d685b36095bff2829daf0af
Viktor Koop <viktor.koop@gmail.com>
2013-07-03 21:43:58 +0200

    add possibility to save server address

---
+++ options.js
@@ -0,0 +1,27 @@
+// Save this script as `options.js`
+
+// Saves options to localStorage.
+function save_options() {
+  var select = document.getElementById("server-address");
+  var server_address = select.value;
+  localStorage["server_address"] = server_address;
+
+  // Update status to let user know options were saved.
+  var status = document.getElementById("status");
+  status.innerHTML = "Options Saved.";
+  setTimeout(function() {
+    status.innerHTML = "";
+  }, 750);
+}
+
+// Restores select box state to saved value from localStorage.
+function restore_options() {
+  var favorite = localStorage["server_address"];
+  if (!favorite) {
+    return;
+  }
+  var select = document.getElementById("server-address");
+  select.value = favorite;
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.querySelector('#save').addEventListener('click', save_options);
