5bf5bb7e620d799c2336c56750488d5bf9c6f084
Viktor Koop <viktor.koop@gmail.com>
2013-07-04 13:27:41 +0200

    reorder structure

--- app/contentscript.js
+++ app/contentscript.js
@@ -3,20 +3,16 @@ port.onMessage.addListener(function(msg){
 
 })
 
-$(document).on("mousedown touchstart", "textarea, input", function(e){
+$(document).on("mousedown touchstart", "textarea, input[type=text]", function(e){
 	//Show qr code
 	var x = e.pageX;
 	var y = e.pageY;
 
-
 	$qrCodeElem = $('#qrcode-icon-ext');
 
 	if($qrCodeElem.length > 0){
-		console.log("exists")
 		$qrCodeElem.show();
-
 	} else {
-		console.log("new")
 		var newdiv = document.createElement('div'); 
 		newdiv.setAttribute('id','qrcode-icon-ext');
 		document.body.appendChild(newdiv)
@@ -33,7 +29,6 @@ $(document).on("mousedown touchstart", "textarea, input", function(e){
 
 	//TODO: generate ID on server
 	$.get("http://localhost:8081/client-register").then(function(response){
-		console.log(response);
 		var address = "http://localhost:8081/qrcode/" + response;
 		$qrCodeElem.html('<img src="' + address +'">');
 
@@ -46,7 +41,6 @@ $(document).on("mousedown touchstart", "textarea, input", function(e){
 	})
 });
 
-$(document).on("mouseup touchend", "textarea, input, #qrcode-icon-ext", function(e){
-	console.log("end");
+$(document).on("mouseup touchend", "textarea, input[type=text], #qrcode-icon-ext", function(e){
 	$('#qrcode-icon-ext').hide();
 });
